4be643e3223204f863c367d65f42e402804c5e8c
Adam Kuhn <adamsparkiq@gmail.com>
2025-06-22 17:19:15 -0400

    Initial Commit

---
+++ SparkIQ/SparkIQApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkIQApp.swift
+//  SparkIQ
+//
+//  Created by Adam K on 6/22/25.
+//
+
+import SwiftUI
+
+@main
+struct SparkIQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
